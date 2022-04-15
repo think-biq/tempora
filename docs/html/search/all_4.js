6a3d3a554d8523118f208db217afaa53de590ef4
blurryroots <blurryroots@posteo.de>
2022-04-15 13:35:22 +0200

    updated docs

--- docs/html/search/all_4.js
+++ docs/html/search/all_4.js
@@ -6,12 +6,13 @@ var searchData=
   ['tempora_5fenv_3',['TEMPORA_ENV',['../common_8h.html#a12a9800d4152308ece6dcc56a46bc081af47fd04080e0f5e539966cf5630f9f46',1,'common.h']]],
   ['tempora_5ferror_4',['TEMPORA_ERROR',['../common_8h.html#a12a9800d4152308ece6dcc56a46bc081ac25cd5aac5e8dc7df532e90ad88d744b',1,'common.h']]],
   ['tempora_5fpath_5fseparator_5fchar_5',['TEMPORA_PATH_SEPARATOR_CHAR',['../common_8h.html#add234e94e7ed8d98f53b608d4fd27f9c',1,'common.h']]],
-  ['tempora_5fpath_5fsize_6',['TEMPORA_PATH_SIZE',['../common_8h.html#a67725f3c4092a42fc115dbd9eee8138d',1,'common.h']]],
-  ['tempora_5fplatform_7',['TEMPORA_PLATFORM',['../common_8h.html#a12a9800d4152308ece6dcc56a46bc081acbe7786aa5da5f849c78f046f173cf4a',1,'common.h']]],
-  ['tempora_5fread_8',['tempora_read',['../read_8h.html#a045d0926ab7e9bec34d84db0cf9bebe4',1,'read.c']]],
-  ['tempora_5fread_5ffrom_5fcwd_9',['tempora_read_from_cwd',['../specific_8h.html#a034864bfe303720bead52c0784de8e99',1,'specific.c']]],
-  ['tempora_5fread_5ffrom_5fenv_10',['tempora_read_from_env',['../specific_8h.html#a7bbdb98401b6c597819ac8275d6bb002',1,'specific.c']]],
-  ['tempora_5fread_5ffrom_5fplatform_5fpath_11',['tempora_read_from_platform_path',['../specific_8h.html#a32ed0924a419056d2eccb765806b872b',1,'specific.c']]],
-  ['tempora_5ftemp_5fdir_5ftype_12',['tempora_temp_dir_type',['../common_8h.html#a12a9800d4152308ece6dcc56a46bc081',1,'common.h']]],
-  ['tempora_5ftemp_5fdir_5ftype_5ft_13',['tempora_temp_dir_type_t',['../common_8h.html#ad9af923e31133a4d00d9b79e831d15e4',1,'common.h']]]
+  ['tempora_5fpath_5fseparator_5fstr_6',['TEMPORA_PATH_SEPARATOR_STR',['../common_8h.html#a8578b6b5d2dbb919d339cc5ee8b9b990',1,'common.h']]],
+  ['tempora_5fpath_5fsize_7',['TEMPORA_PATH_SIZE',['../common_8h.html#a67725f3c4092a42fc115dbd9eee8138d',1,'common.h']]],
+  ['tempora_5fplatform_8',['TEMPORA_PLATFORM',['../common_8h.html#a12a9800d4152308ece6dcc56a46bc081acbe7786aa5da5f849c78f046f173cf4a',1,'common.h']]],
+  ['tempora_5fread_9',['tempora_read',['../read_8h.html#a045d0926ab7e9bec34d84db0cf9bebe4',1,'read.c']]],
+  ['tempora_5fread_5ffrom_5fcwd_10',['tempora_read_from_cwd',['../specific_8h.html#a034864bfe303720bead52c0784de8e99',1,'specific.c']]],
+  ['tempora_5fread_5ffrom_5fenv_11',['tempora_read_from_env',['../specific_8h.html#a7bbdb98401b6c597819ac8275d6bb002',1,'specific.c']]],
+  ['tempora_5fread_5ffrom_5fplatform_5fpath_12',['tempora_read_from_platform_path',['../specific_8h.html#a32ed0924a419056d2eccb765806b872b',1,'specific.c']]],
+  ['tempora_5ftemp_5fdir_5ftype_13',['tempora_temp_dir_type',['../common_8h.html#a12a9800d4152308ece6dcc56a46bc081',1,'common.h']]],
+  ['tempora_5ftemp_5fdir_5ftype_5ft_14',['tempora_temp_dir_type_t',['../common_8h.html#ad9af923e31133a4d00d9b79e831d15e4',1,'common.h']]]
 ];
--- docs/html/search/defines_0.js
+++ docs/html/search/defines_0.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['tempora_5fpath_5fseparator_5fchar_0',['TEMPORA_PATH_SEPARATOR_CHAR',['../common_8h.html#add234e94e7ed8d98f53b608d4fd27f9c',1,'common.h']]],
-  ['tempora_5fpath_5fsize_1',['TEMPORA_PATH_SIZE',['../common_8h.html#a67725f3c4092a42fc115dbd9eee8138d',1,'common.h']]]
+  ['tempora_5fpath_5fseparator_5fstr_1',['TEMPORA_PATH_SEPARATOR_STR',['../common_8h.html#a8578b6b5d2dbb919d339cc5ee8b9b990',1,'common.h']]],
+  ['tempora_5fpath_5fsize_2',['TEMPORA_PATH_SIZE',['../common_8h.html#a67725f3c4092a42fc115dbd9eee8138d',1,'common.h']]]
 ];
